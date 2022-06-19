6297f02048be0cf9dcb0189192836da58743e22d
JiYoung <yj0zero@gmail.com>
2022-06-19 20:27:51 +0900

    Create backgroundImg.js

---
+++ JS/backgroundImg.js
@@ -0,0 +1,14 @@
+const images = [
+    "0.jpg",
+    "1.jpg",
+    "2.jpg"
+];
+
+const back =document.querySelector(".bgimg");
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `./img/${chosenImage}`;
+
+back.appendChild(bgImage);
